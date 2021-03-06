4c8bd065397e8bc1de18008d3b6969f327e35673
Dalnogirl <velikiizevs@gmail.com>
2021-03-06 22:19:55 +0300

    feat: added feed response typization

--- src/services/FeedService.ts
+++ src/services/FeedService.ts
@@ -2,17 +2,17 @@
 import { call } from 'redux-saga/effects';
 import { ApiRoute } from '../constants/paths';
 import { http } from './http.service';
+import { AxiosResponse } from 'axios';
+import Word from '../models/word';
 
 export const FeedService = {
-  *fetchFeed(offset: number, limit?: number) {
+  * fetchFeed(offset: number, limit?: number) {
     const params = new URLSearchParams();
     params.append('offset', offset.toString());
     if (limit !== undefined) {
       params.append('limit', limit.toString());
     }
-
-    const response = yield call(http(false).get, ApiRoute.DictionaryFeed);
-    console.log(response);
+    const response: AxiosResponse<Word[]> = yield call(http(false).get, ApiRoute.DictionaryFeed);
     return response.data;
   },
 };
